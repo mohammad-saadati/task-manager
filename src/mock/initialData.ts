80f8253c8b1cd43d551165e83f81e78ca14c69ad
mohammad_saadati <saadati.m.b@gmail.com>
2023-02-25 22:08:47 +0330

    change initial data structure

--- src/mock/initialData.ts
+++ src/mock/initialData.ts
@@ -3,6 +3,20 @@ interface boardMockData {
   columns: { [id: string]: { id: string; title: string; taskIds: string[] } };
   columnOrder: string[];
 }
+export interface Task {
+  id: string;
+  content: string;
+}
+export interface Column {
+  id: string;
+  title: string;
+  tasks: Task[];
+  tasksOrder: string[];
+}
+export interface boardMockData2 {
+  columns: Column[];
+  columnOrder: string[];
+}
 
 const initialData: boardMockData = {
   tasks: {
@@ -28,6 +42,41 @@ const initialData: boardMockData = {
       taskIds: [],
     },
   },
+  columnOrder: ["column-1", "column-2", "column-3"],
+};
+export const initialData2: boardMockData2 = {
+  columns: [
+    {
+      id: "column-1",
+      title: "Done",
+      tasks: [
+        { id: "task-1", content: "task-1 content" },
+        { id: "task-2", content: "task-2 content" },
+        { id: "task-3", content: "task-3 content" },
+      ],
+      tasksOrder: ["task-1", "task-2", "task-3"],
+    },
+    {
+      id: "column-2",
+      title: "Backlog",
+      tasks: [
+        { id: "task-4", content: "task-4 content" },
+        { id: "task-5", content: "task-5 content" },
+        { id: "task-6", content: "task-6 content" },
+      ],
+      tasksOrder: ["task-4", "task-5", "task-6"],
+    },
+    {
+      id: "column-3",
+      title: "Backlog",
+      tasks: [
+        { id: "task-7", content: "task-7 content" },
+        { id: "task-8", content: "task-8 content" },
+        { id: "task-9", content: "task-9 content" },
+      ],
+      tasksOrder: ["task-7", "task-8", "task-9"],
+    },
+  ],
   columnOrder: ["column-1", "column-2", "column-3"],
 };
 
