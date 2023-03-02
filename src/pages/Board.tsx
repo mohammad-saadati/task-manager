483bde07893f169eafc9cc4c15bec86fde64d489
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-02 10:13:54 +0330

    initial board page

---
+++ src/pages/Board.tsx
@@ -0,0 +1,27 @@
+import { FC, useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../utils/axios";
+
+const Board = () => {
+  const [board, setBoard] = useState({});
+  let { id } = useParams();
+
+  const getData = useCallback(async () => {
+    try {
+      const url = `/boards/${id}`;
+      const res = await api.get(url);
+      const { data } = res;
+      console.log("res", res);
+
+      setBoard(data.board);
+    } catch (error) {}
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  return <div>Board Page</div>;
+};
+
+export default Board;
