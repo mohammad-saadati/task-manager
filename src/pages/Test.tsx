a757e1fae5eded33d9c82330c18e8ddfe5be89c8
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-03 18:16:26 +0330

    some optimizing checks

---
+++ src/pages/Test.tsx
@@ -0,0 +1,40 @@
+import { FC, memo, useMemo, useState } from "react";
+
+interface propTypes {
+  title: string[];
+}
+
+const Bache = memo(
+  ({ title }: propTypes) => {
+    return <div>{title}</div>;
+  },
+  (prev, next) => {
+    return prev.title.length === next.title.length;
+  }
+);
+
+const WrapperComponent = () => {
+  const [title, setTitle] = useState(["sample test"]);
+  const [flag, setFlag] = useState(false);
+
+  const computed = useMemo(() => {
+    console.log("usememo called");
+    return title;
+  }, [title]);
+
+  const handleClick = () => {
+    setTitle((current) => {
+      let temp = Array.from(current);
+      temp = ["sample test"];
+      return temp;
+    });
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>reassing string title</button>
+      <Bache title={title}></Bache>
+    </div>
+  );
+};
+
+export default WrapperComponent;
