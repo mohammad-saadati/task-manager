5388c0feb41ef19764ccf2ffb3f1bf17d5d388ae
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-11 16:36:50 +0330

    hide parts on login page

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -3,15 +3,24 @@ import { FC, ReactNode } from "react";
 import Sidebar from "../components/Sidebar";
 import Appbar from "../components/Appbar";
 
+import { useLocation } from "react-router-dom";
+
 export interface Props {
   children?: ReactNode;
 }
 
 const defaultLayout: FC<Props> = ({ children }) => {
+  const location = useLocation();
+
   return (
     <div>
-      <Appbar />
-      <Sidebar />
+      {location.pathname !== "/login" ? (
+        <>
+          <Appbar />
+          <Sidebar />
+        </>
+      ) : null}
+
       <div className="py-20 px-6">{children}</div>
     </div>
   );
