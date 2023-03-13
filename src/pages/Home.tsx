ada86844577b3979b1257aae4c6f759426612cd4
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-13 13:38:33 +0330

    remove canvas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import { userDetails } from "../Types/props";
-import Canvas from "../components/CanvasBoard";
 import TaskManagment from "./TaskManagment";
 interface homeData {
   userDetails: any;
