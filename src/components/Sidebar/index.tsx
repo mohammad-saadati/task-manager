a4cddd379f3eb973b706e5ac8ccc2ca3a293d25b
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-22 13:32:37 +0430

    make drawer persistent

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,16 @@
-import { Box, Drawer } from "@mui/material";
+import { Box, Divider, Drawer, IconButton } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+// icons
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+// store
 import { useAppSelector, useAppDispatch } from "../../store/hooks";
 import { closeDrawer } from "../../store/features/drawer";
+//
 import Content from "./content";
 
 const Sidebar = () => {
+  const theme = useTheme();
   const dispatcher = useAppDispatch();
   const drawerState = useAppSelector((state) => state.drawer);
 
@@ -13,9 +20,19 @@ const Sidebar = () => {
         anchor="left"
         open={drawerState.drawerIsOpen}
         onClose={() => dispatcher(closeDrawer())}
-        variant="temporary"
+        variant="persistent"
         sx={{ maxWidth: drawerState.drawerWidth }}
       >
+        <div className="flex justify-end">
+          <IconButton onClick={() => dispatcher(closeDrawer())}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
         <Content />
       </Drawer>
     </Box>
