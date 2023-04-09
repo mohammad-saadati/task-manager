9ed30a0520f07f33dca92ed6474f851deedce75e
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-09 17:18:24 +0430

    dialog title

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
 import { useAppSelector } from "../../store/hooks";
+import { Dialog } from "@mui/material";
+import ModalTitle from "./ModalTitle";
 
 const Modal: FC = () => {
   const modalIsOpen = useAppSelector((state) => state.modal.modalIsOpen);
 
   if (modalIsOpen) {
-    return ;
+    return (
+      <Dialog open={modalIsOpen}>
+        <ModalTitle>Task detail</ModalTitle>
+      </Dialog>
+    );
   } else {
     return <></>;
   }
