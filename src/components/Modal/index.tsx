226f8ba1cca1c1c112ed951a86677d4392c1345b
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-21 03:14:59 +0430

    import slate

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -11,6 +11,26 @@ import ModalTitle from "./ModalTitle";
 // slatejs editor
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
+// editor types
+import { BaseEditor, Descendant } from "slate";
+import { ReactEditor } from "slate-react";
+
+type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomText = { text: string };
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+}
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
 
 const Modal: FC = () => {
   const modalIsOpen = useAppSelector((state) => state.modal.modalIsOpen);
