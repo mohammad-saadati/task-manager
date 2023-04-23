4467ba87ab8f3d737575b9b997b19d13c231691e
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-23 15:10:19 +0430

    create page

---
+++ src/pages/EditorTest.tsx
@@ -0,0 +1,11 @@
+import { useCallback, useEffect, useState, useRef, useMemo } from "react";
+import Editor from "../components/Editor";
+
+const EditorTest = () => {
+  return (
+    <div className="mt-32">
+      <Editor />
+    </div>
+  );
+};
+export default EditorTest;
