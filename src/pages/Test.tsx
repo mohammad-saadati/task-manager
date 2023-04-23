99aa93bae8f7543eeba05176cbd263e3800f62d5
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-23 14:42:38 +0430

    how clean up function works

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,14 +1,24 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 
 const Test = () => {
   const [s, setS] = useState(0);
 
+  const unmountRef = useRef(false);
   console.log("rerenders");
 
   const main = () => {
     setS(s + 10);
   };
   const inc = useCallback(main, [s]);
+
+  useEffect(() => {
+    // if (unmountRef.current) return;
+    console.log("useEffect body");
+    return () => {
+      console.log("clean function");
+      unmountRef.current = true;
+    };
+  }, [s]);
 
   return (
     <div className="mt-32" onClick={main}>
