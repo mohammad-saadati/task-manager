89295a5346c3dbf728a17fc77e1ae4689e775648
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-23 15:10:36 +0430

    set up slate component

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,3 +1,15 @@
-const RichTextEditor = () => {};
+import { useMemo } from "react";
+// slate
+import { createEditor } from "slate";
+import { withReact, Slate } from "slate-react";
+import { withHistory } from "slate-history";
+//
+import initialValue from "./initialValue";
+
+const RichTextEditor = () => {
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
+  return <Slate editor={editor} value={initialValue}></Slate>;
+};
 
 export default RichTextEditor;
