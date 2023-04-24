cafe66ff859fe77ea4ba8cb0aa5ea9425cd3a6a8
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-24 11:16:24 +0430

    editor toolbar

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react";
+import { Toolbar } from "./Toolbar";
 // slate
 import { createEditor } from "slate";
 import { withReact, Slate } from "slate-react";
@@ -9,7 +10,11 @@ import initialValue from "./initialValue";
 const RichTextEditor = () => {
   const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
-  return <Slate editor={editor} value={initialValue}></Slate>;
+  return (
+    <Slate editor={editor} value={initialValue}>
+      <Toolbar></Toolbar>
+    </Slate>
+  );
 };
 
 export default RichTextEditor;
