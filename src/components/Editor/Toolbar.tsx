cfbf47fbb154ea75605d64d9876dcdba78e403a0
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-24 12:26:39 +0430

    compose toolbar

--- src/components/Editor/Toolbar.tsx
+++ src/components/Editor/Toolbar.tsx
@@ -1,3 +1,30 @@
-import React from "react";
+import React, { Ref, PropsWithChildren } from "react";
+//
+import { cx, css } from "@emotion/css";
 
-export const Toolbar = React.forwardRef(() => (<div></div>));
+interface BaseProps {
+  className: string;
+  [key: string]: unknown;
+}
+
+export const Toolbar = React.forwardRef(
+  (
+    { className, ...props }: PropsWithChildren<BaseProps>,
+    ref: Ref<OrNull<HTMLDivElement>>
+  ) => (
+    <Menu
+      {...props}
+      ref={ref}
+      className={cx(
+        className,
+        css`
+          position: relative;
+          padding: 1px 18px 17px;
+          margin: 0 -20px;
+          border-bottom: 2px solid #eee;
+          margin-bottom: 20px;
+        `
+      )}
+    />
+  )
+);
