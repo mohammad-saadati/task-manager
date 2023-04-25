da844afab611b6c162beaf20ffc4bd9d25e33444
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-25 15:21:21 +0430

    other render blocks

--- src/components/Editor/Element.tsx
+++ src/components/Editor/Element.tsx
@@ -27,6 +27,30 @@ const Element = ({ attributes, children, element }: ElementProps) => {
           {children}
         </ul>
       );
+    case "heading-one":
+      return (
+        <h1 style={style} {...attributes}>
+          {children}
+        </h1>
+      );
+    case "heading-two":
+      return (
+        <h2 style={style} {...attributes}>
+          {children}
+        </h2>
+      );
+    case "list-item":
+      return (
+        <li style={style} {...attributes}>
+          {children}
+        </li>
+      );
+    case "numbered-list":
+      return (
+        <ol style={style} {...attributes}>
+          {children}
+        </ol>
+      );
     default:
       return (
         <p style={style} {...attributes}>
