5d458f7169523dc34aef9b0ea100357703746c93
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-25 11:35:08 +0430

    import required modules

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,18 +1,20 @@
-import { useMemo } from "react";
-import { Toolbar } from "./Toolbar";
+import { useMemo, useCallback } from "react";
+import { Toolbar } from "./EditorMenu";
 // slate
 import { createEditor } from "slate";
-import { withReact, Slate } from "slate-react";
+import { Editable, withReact, Slate } from "slate-react";
 import { withHistory } from "slate-history";
 //
 import initialValue from "./initialValue";
 
 const RichTextEditor = () => {
+  // const renderElement = useCallback((props) => <Element {...props} />, []);
   const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
   return (
     <Slate editor={editor} value={initialValue}>
       <Toolbar></Toolbar>
+      <Editable renderElement={renderElement} />
     </Slate>
   );
 };
