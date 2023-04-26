0b98e1d3bb9f42e6ea664917efd0e6f872adfe92
mohammad_saadati <saadati.m.b@gmail.com>
2023-04-26 10:54:38 +0430

    renderLeaf prop

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -8,15 +8,17 @@ import { withHistory } from "slate-history";
 import initialValue from "./initialValue";
 //
 import Element from "./Element";
+import Leaf from "./Leaf";
 
 const RichTextEditor = () => {
   const renderElement = useCallback((props) => <Element {...props} />, []);
   const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
 
   return (
     <Slate editor={editor} value={initialValue}>
       <Toolbar></Toolbar>
-      <Editable renderElement={renderElement} />
+      <Editable renderElement={renderElement} renderLeaf={renderLeaf} />
     </Slate>
   );
 };
