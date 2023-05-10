07f3db12d280ba36d12ba2d8fd2552a6251fd4ed
mohammad_saadati <saadati.m.b@gmail.com>
2023-05-11 02:42:11 +0430

    button component

---
+++ src/components/Editor/Buttons.tsx
@@ -0,0 +1,14 @@
+const MarkButton = ({ format, icon }) => {
+  const editor = useSlate();
+  return (
+    <Button
+      active={isMarkActive(editor, format)}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleMark(editor, format);
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  );
+};
